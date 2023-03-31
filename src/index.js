8b8be40a981fb4cc40418b056b7bc4b725096d9c
NastiaAnn <anastasianazar4@gmail.com>
2023-03-31 12:36:21 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import './css/styles.css';
 import { fetchCountries } from './fetchCountries';
-
 import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
@@ -8,7 +7,7 @@ const DEBOUNCE_DELAY = 300;
 
 const input = document.querySelector('#search-box');
 
-input.addEventListener('input', handleInputSearch);
+input.addEventListener('input', debounce(handleInputSearch, 300));
 
 function handleInputSearch(event) {
   event.preventDefault();
